d62aac4f2b0d4a32e7142a3a539ad9b74609b1b3
janette-wong <janettewongprasith@gmail.com>
2022-05-02 22:41:03 +0700

    [ADD] content update

---
+++ rate_me/lib/screens/content.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class ContentChoice extends StatefulWidget {
+  const ContentChoice({Key? key}) : super(key: key);
+
+  @override
+  State<ContentChoice> createState() => _ContentChoice();
+}
+
+class _ContentChoice extends State<ContentChoice> {
+  final List<String> movies = [
+    'Comedy',
+    'Horror',
+    'Old School',
+    'Adventure',
+    'Drama',
+    'Funny',
+    'Sci-fi',
+    'Action',
+    'Romance'
+  ];
+  List<String> selectedReportList = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Which type of content you interest in',
+              style: TextStyle(color: Colors.black, fontSize: 18.0),
+            ),
+            SizedBox(
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+            ),
+            MultiSelectChip(
+              movies,
+              onSelectionChanged: (selectedList) {
+                setState(() {
+                  selectedReportList = selectedList;
+                });
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 32.0),
+              child: RaisedButton(
+                  color: const Color.fromARGB(255, 0, 48, 137),
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(
+                        color: Color(0xffffffff),
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MultiSelectChip extends StatefulWidget {
+  final List<String> typeList;
+  final Function(List<String>) onSelectionChanged;
+
+  MultiSelectChip(this.typeList, {required this.onSelectionChanged});
+
+  @override
+  _MultiSelectChipState createState() => _MultiSelectChipState();
+}
+
+class _MultiSelectChipState extends State<MultiSelectChip> {
+  // String selectedChoice = "";
+  List<String> selectedChoices = [];
+
+  _buildChoiceList() {
+    List<Widget> choices = [];
+
+    for (var item in widget.typeList) {
+      choices.add(
+        Container(
+        padding: const EdgeInsets.all(2.0),
+        child: ChoiceChip(
+          label: Text(item),
+          labelStyle: TextStyle(
+              fontSize: 14.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          backgroundColor: Color(0xffededed),
+          selectedColor: Color.fromARGB(255, 230, 76, 76),
+          selected: selectedChoices.contains(item),
+          onSelected: (selected) {
+            setState(() {
+              selectedChoices.contains(item)
+                  ? selectedChoices.remove(item)
+                  : selectedChoices.add(item);
+              widget.onSelectionChanged(selectedChoices);
+            });
+          },
+        ),
+      ));
+    }
+
+    return choices;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: _buildChoiceList(),
+    );
+  }
+}
